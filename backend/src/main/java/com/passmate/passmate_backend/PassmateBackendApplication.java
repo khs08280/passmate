47ed34b2e5f9d5b45772677bbea352ce34ac0612
khs08280 <u6959@naver.com>
2025-08-25 22:09:00 +0900

    init: 프로젝트 설정

---
+++ backend/src/main/java/com/passmate/passmate_backend/PassmateBackendApplication.java
@@ -0,0 +1,13 @@
+package com.passmate.passmate_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication()
+public class PassmateBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PassmateBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/passmate/passmate_backend/PassmateBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.passmate.passmate_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PassmateBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
